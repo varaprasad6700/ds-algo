dffed7953a44830fb17bd23d395103f9a8dd4a77
varaprasad <pothulavaraprasad333@gmail.com>
2022-06-15 16:34:44 +0530

    update maximum_subarray.py with starting and ending indices to be sent and changed their tests

--- src/ds/maximum_subarray.py
+++ src/ds/maximum_subarray.py
@@ -14,5 +14,25 @@ def max_sub_array(nums: List[int]) -> int:
     return max_sum
 
 
+def max_sub_array_with_start_and_end(nums: List[int]) -> List[int]:
+    max_sum = nums[0]
+    current_sum = nums[0]
+    start = 0
+    end = 0
+    temp_start = 0
+    for i in range(1, len(nums)):
+        if nums[i] > current_sum + nums[i]:
+            temp_start = i
+            current_sum = max(nums[i], current_sum + nums[i])
+        else:
+            current_sum = max(nums[i], current_sum + nums[i])
+        if current_sum > max_sum:
+            start = temp_start
+            end = i
+            max_sum = max(max_sum, current_sum)
+    return [max_sum, start, end]
+
+
 if __name__ == '__main__':
     print(max_sub_array([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+    print(max_sub_array_with_start_and_end([5, 4, -1, 7, 8]))
--- tests/test_maximum_subarray.py
+++ tests/test_maximum_subarray.py
@@ -2,13 +2,15 @@ from typing import List
 
 import pytest
 
-from src.ds.maximum_subarray import max_sub_array
+from src.ds.maximum_subarray import max_sub_array, max_sub_array_with_start_and_end
 
 
 @pytest.mark.parametrize("numbers, expected", [
-    ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
-    ([1], 1),
-    ([5, 4, -1, 7, 8], 23)
+    ([-2, 1, -3, 4, -1, 2, 1, -5, 4], [6, 3, 6]),
+    ([1], [1, 0, 0]),
+    ([5, 4, -1, 7, 8], [23, 0, 4]),
+    ([5, 4, -10, 7, 8], [15, 3, 4])
 ])
-def test_maximum_subarray(numbers: List[int], expected: int):
-    assert max_sub_array(numbers) == expected
+def test_maximum_subarray(numbers: List[int], expected: List[int]):
+    assert max_sub_array(numbers) == expected[0]
+    assert max_sub_array_with_start_and_end(numbers) == expected
