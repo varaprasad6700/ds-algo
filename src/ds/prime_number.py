709ffae9023f9cb2303ae91b526f100f29b657ab
varaprasad <pothulavaraprasad333@gmail.com>
2022-06-16 10:26:10 +0530

    changed prime_number.py example with a list of numbers

--- src/ds/prime_number.py
+++ src/ds/prime_number.py
@@ -4,7 +4,7 @@ from math import sqrt
 def is_prime(num: int) -> bool:
     if num == 2:
         return True
-    elif num % 2 == 0:
+    elif num % 2 == 0 or num <= 1:
         return False
     else:
         for j in range(3, int(sqrt(num)) + 1):
@@ -19,3 +19,5 @@ if __name__ == '__main__':
     print(is_prime(3))
     print(is_prime(4))
     print(is_prime(1000))
+    for i in range(100):
+        print("[{} {}]".format(i, is_prime(i)), end=",")
