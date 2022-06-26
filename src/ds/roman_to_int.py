6379a6af8113d7adddb9aa0f8a897734a4586f41
varaprasad <varaprasad.p@techsophy.com>
2022-06-26 22:08:12 +0530

    roman to int

---
+++ src/ds/roman_to_int.py
@@ -0,0 +1,26 @@
+"""
+https://leetcode.com/problems/roman-to-integer/
+"""
+
+
+def roman_to_int(s: str) -> int:
+    order = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+    vals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    out_ = 0
+    ind = 0
+    while ind < len(s):
+        curr = s[ind]
+        if ind >= len(s) - 1:
+            out_ += vals[curr]
+        else:
+            nex = s[ind+1]
+            if order.index(nex) > order.index(curr):
+                out_ -= vals[curr]
+            else:
+                out_ += vals[curr]
+        ind += 1
+    return out_
+
+
+if __name__ == '__main__':
+    print(roman_to_int("MCMXCIV"))
---
+++ tests/test_roman_to_int.py
@@ -0,0 +1,11 @@
+import pytest
+from src.ds.roman_to_int import roman_to_int
+
+
+@pytest.mark.parametrize("roman, expected", [
+    ("MCMXCIV", 1994),
+    ("III", 3),
+    ("LVIII", 58),
+])
+def test_roman_to_int(roman, expected):
+    assert roman_to_int(roman) == expected
