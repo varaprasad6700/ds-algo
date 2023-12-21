ba9a93a404030ea9d6f74bc451ce4f8212808a21
Varaprasad <47860898+varaprasad6700@users.noreply.github.com>
2023-12-21 19:43:58 +0530

    fixed imports

--- src/ds/widest_vertical_area.py
+++ src/ds/widest_vertical_area.py
@@ -1,6 +1,8 @@
 """
 https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points
 """
+from typing import List
+
 def max_width_of_vertical_area(points: List[List[int]]) -> int:
         temp = map(sorted(points, key=lambda x: x[0]), lambda x: x[0])
         return max(j - i for (i, j) in zip(temp[:-1], temp[1:]))
